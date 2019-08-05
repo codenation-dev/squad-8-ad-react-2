7d3f75ad2d9c66786fc35a1f8d65e615b6143c36
daviwesley <davi.wesley@icloud.com>
2019-08-05 19:34:54 -0300

    add styled components and custom components

---
+++ src/components/Input/index.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Input = styled.input.attrs({ type: "text" })`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid #085191;
+  color: #085191;
+  width: 300px;
+  height: 20px;
+`;
+
+export default Input;
