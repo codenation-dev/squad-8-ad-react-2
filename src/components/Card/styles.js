5f8c7b5c202bfbac86bb1660b2252462040dd67f
daviwesley <davi.wesley@icloud.com>
2019-08-10 14:27:12 -0300

    change margin

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -4,7 +4,7 @@ export const CardBox = styled.div`
   width: 350px;
   min-height: 125px;
   word-wrap: break-word;
-  margin-top: 2px;
+  margin: 10px;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   border-radius: 4px;
 `;
